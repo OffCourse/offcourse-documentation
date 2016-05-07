1b1b0f53ca87b1828a127c43dff3b3159d18fe06
Zaturrby <rjkorteschiel@gmail.com>
2016-05-07 14:54:56 +0200

    knowledge on how to use the configuration graphs

--- source-cljcss/offcourse/styles/shame.clj
+++ source-cljcss/offcourse/styles/shame.clj
@@ -6,8 +6,11 @@
              [arithmetic :refer [/ + *]]
              [selectors :as s]]))
 
+; Shame file to style the mailchimp form - o horror - with ID's
+; Nice exercise to get a feel for this garden CSS
 (defn shame [{:keys [templates breakpoints colors units fonts]}]
   [[:#mce-EMAIL {:background :red
                  :width (* (:full units) 10)}]
-   [:#mce-EMAIL-LABEL {:background :red
-                       :subtitle fonts}]])
+   [:#mce-EMAIL-LABEL (merge {:background :red}
+                             (:banner templates))]])
+
