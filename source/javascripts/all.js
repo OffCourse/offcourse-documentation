6d8043bdd0c190bf28180f47abbdbcddf37d9c5e
Zaturrby <rjkorteschiel@gmail.com>
2016-05-23 21:17:53 +0200

    working on anchor

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -2,17 +2,20 @@
 // $( '#nav-sticky' ).fixedsticky();
 
 // Anchorscroll
-// $(function() {
-//   $('a[href*="#"]:not([href="#"])').click(function() {
-//     if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-//       var target = $(this.hash);
-//       target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-//       if (target.length) {
-//         $('html, body').animate({
-//           scrollTop: target.offset().top
-//         }, 1000);
-//         return false;
-//       }
-//     }
-//   });
-// });
+$(function() {
+  console.log("startgin")
+  $('a[href*="#"]:not([href="#"])').bind('click', function(e) {
+    console.log("running")
+    e.preventDefault;
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
