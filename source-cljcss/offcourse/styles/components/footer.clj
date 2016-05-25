2e0c1304687d4164f657a6c1fd5cac02840c592f
Zaturrby <rjkorteschiel@gmail.com>
2016-05-25 14:04:54 +0200

    buttons change

--- source-cljcss/offcourse/styles/components/footer.clj
+++ source-cljcss/offcourse/styles/components/footer.clj
@@ -2,28 +2,29 @@
   (:refer-clojure :exclude [first last + - * /])
   (:require [garden
               [arithmetic :refer [* + - /]]
-              [units :as u :refer [vh percent]]]
+              [units :as u :refer [vh percent]]
+              [selectors :as s]]
             [offcourse.styles.vocabulary :as v]))
 
 (defn footer [{:keys [templates colors units borders]}]
   [[v/footer (merge (:highlighted templates)
-                    {
+                    {:padding [[(:three units) 0]]
                      :background (:night colors)
                      :color (:day colors)
                       })
     [v/footer--container (merge (:row-component templates)
-                                {:align-items :flex-start
-                                 :padding [[(:four units) 0 (:four units) (* (:atom units) 35)]]})]]
+                                {:align-items :flex-start})]]
    [v/footer--textbar (merge (:textbar templates)
                              (:title templates)
                              {:flex-direction :row
                               :font-size       (:subtitle-font units)
-                              :padding [[(:half units) (:full units)]]
-                              :border-bottom (:default borders)
+                              :padding [[(:third units) (:half units)]]
+                              :border [[:none :none (:default borders) :none]]
                               :margin-right (:full units)
                               :margin-bottom (:full units)
                               :background (:day colors)
-                              :color (:night colors)})
+                              :color (:night colors)
+                              :cursor :pointer})
     [v/hovered (:highlighted borders)]]
    [v/footer--icon {:height (:full units)
                     :width  (:full units)
